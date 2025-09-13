9b3ee8c092f4866b4fc45a74b6274cea898af07a
Shibam <shibamdeyroy2860@gmail.com>
2025-09-13 17:25:17 +0000

    email password sign in working but google sign is not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "studio-587186531-f420f",
-  appId: "1:152835950010:web:179e359b4d92ad9ffec983",
-  storageBucket: "studio-587186531-f420f.firebasestorage.app",
   apiKey: "AIzaSyD0oAxqQZ8A0elMr_D3-MAxiiD8kMDHQU4",
-  authDomain: "studio-587186531-f420f.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "152835950010"
+  authDomain: "studio-587186531-f420f.web.app",
+  projectId: "studio-587186531-f420f",
+  storageBucket: "studio-587186531-f420f.appspot.com",
+  messagingSenderId: "152835950010",
+  appId: "1:152835950010:web:179e359b4d92ad9ffec983"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
